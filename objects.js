cc54dd283fa8c6f6fa513c0df1ccbb5cbfa917ea
devin williams <williams.devin702@gmail.com>
2020-06-11 06:06:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
 var playlist = {drake: 'toosie slide'};
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.drake = 'Slowdive'; 'My Bloody Valentine';'Phil Ochs'
-  return playlist
+  playlist['drake'] = 'Slowdive'; 'My Bloody Valentine';'Phil Ochs'
 }
